c351eeb1e40e641918aaa41e906c5244d788640c
garciart <rgarcia@rgprogramming.com>
2019-11-04 14:30:54 -0500

    Added and tested database support. Need to fix database structure to match styling guide

--- QuickMeds/QuickMeds/AboutPage.xaml.cs
+++ QuickMeds/QuickMeds/AboutPage.xaml.cs
@@ -9,21 +9,23 @@ namespace QuickMeds {
         public AboutPage() {
             InitializeComponent();
             headerImage.Source = ImageSource.FromResource("QuickMeds.Assets.headerImage_v1.png");
-            CopyrightLabel.Text = String.Format(AppResources.CopyrightLabel, DateTime.Now.Year.ToString());
+            CopyrightLabel.Text = string.Format(AppResources.CopyrightLabel, DateTime.Now.Year.ToString());
         }
 
         private async void UpdateDatabaseButton_Clicked(object sender, EventArgs e) {
-            // await this.DisplayAlert("Quick Meds", "You want to update the database!", "OK");
-            string databaseFile = "medications.db";
-            string databaseURL = "https://raw.githubusercontent.com/garciart/QuickMeds/master/Database/" + databaseFile;
-            try {
-                byte[] returnedBytes = await AppFunctions.DownloadFileAsync(databaseURL);
-                File.WriteAllBytes(String.Format("{0}/{1}", Constants.AppDataPath, databaseFile), returnedBytes);
-                await this.DisplayAlert("Quick Meds", AppResources.DownloadSuccessMessage, "OK");
-                await Application.Current.MainPage.Navigation.PopAsync();
-            }
-            catch (Exception ex) {
-                await this.DisplayAlert("Quick Meds", String.Format(AppResources.DownloadErrorMessage, ex.Message), "OK");
+            bool answer = await DisplayAlert("Quick Meds", AppResources.UpdateConfirm, AppResources.ButtonYes, AppResources.ButtonNo);
+            if (answer) {
+                string databaseFile = "medications.db";
+                string databaseURL = "https://raw.githubusercontent.com/garciart/QuickMeds/master/Database/" + databaseFile;
+                try {
+                    byte[] returnedBytes = await AppFunctions.DownloadFileAsync(databaseURL);
+                    File.WriteAllBytes(string.Format("{0}/{1}", Constants.AppDataPath, databaseFile), returnedBytes);
+                    await this.DisplayAlert("Quick Meds", AppResources.DownloadSuccessMessage, "OK");
+                    await Application.Current.MainPage.Navigation.PopAsync();
+                }
+                catch (Exception ex) {
+                    await this.DisplayAlert("Quick Meds", string.Format(AppResources.DownloadErrorMessage, ex.Message), "OK");
+                }
             }
         }
     }
--- QuickMeds/QuickMeds/Common/DataFunctions.cs
+++ QuickMeds/QuickMeds/Common/DataFunctions.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SQLite;
 using QuickMeds.Models;
+using Action = QuickMeds.Models.Action;
 
 namespace QuickMeds.Data {
     public class DataFunctions {
@@ -11,11 +12,47 @@ namespace QuickMeds.Data {
 
         public DataFunctions(string dbPath) {
             _database = new SQLiteAsyncConnection(dbPath);
-            _database.CreateTableAsync<Medication>().Wait();
+            _database.CreateTableAsync<MEDICATIONS>().Wait();
         }
 
-        public Task<List<Medication>> GetMedicationsAsync() {
-            return _database.Table<Medication>().ToListAsync();
+        public Task<List<MEDICATIONS>> GetMedicationsAsync() {
+            return _database.Table<MEDICATIONS>().ToListAsync();
+        }
+
+        public Task<MEDICATIONS> GetMedicationsAsync(int MEDID) {
+            return _database.Table<MEDICATIONS>()
+                            .Where(i => i.MEDID == MEDID)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<List<Condition>> GetConditionsAsync() {
+            return _database.Table<Condition>().ToListAsync();
+        }
+
+        public Task<Condition> GetConditionAsync(int conditionID) {
+            return _database.Table<Condition>()
+                            .Where(i => i.ConditionID == conditionID)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<List<Action>> GetActionAsync() {
+            return _database.Table<Action>().ToListAsync();
+        }
+
+        public Task<Action> GetActionAsync(int actionID) {
+            return _database.Table<Action>()
+                            .Where(i => i.ActionID == actionID)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<List<Control>> GetControlAsync() {
+            return _database.Table<Control>().ToListAsync();
+        }
+
+        public Task<Control> GetControlAsync(int controlID) {
+            return _database.Table<Control>()
+                            .Where(i => i.ControlID == controlID)
+                            .FirstOrDefaultAsync();
         }
     }
 }
