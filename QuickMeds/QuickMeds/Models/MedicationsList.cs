f4da3ac50a70413e4eaf63de10dfa2784d028fbe
garciart <rgarcia@rgprogramming.com>
2019-11-06 23:22:32 -0500

    Refining medication data to screen process. Will continue in the AM

--- QuickMeds/QuickMeds/Models/MedicationsList.cs
+++ QuickMeds/QuickMeds/Models/MedicationsList.cs
@@ -7,5 +7,7 @@ namespace QuickMeds.Models {
         public string MedicationName { get; set; }
         public string MedicationAKA { get; set; }
         public string MedicationConditions { get; set; }
+        public string MedicationType { get; set; }
+        public string MedicationListItem { get { return string.Format("{0} ({1}) AKA {2} ({3})", MedicationName, MedicationType, MedicationAKA, MedicationType == "B" ? "G" : "B"); } }
     }
 }
